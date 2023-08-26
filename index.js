a5b954132f0e158fe8600066913c03c2dfce6d97
it21175848 <it21175848@my.sliit.lk>
2023-08-27 03:10:17 +0530

    modify the javascript code

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ function updateClock(){
         ampm = "PM";
     }
 
+    h = h<10 ? "0" + h : h;
+    m = m<10 ? "0" + m : m;
+    s = s<10 ? "0" + s : s;
+
+
     hour1.innerText = h;
     minuteE1.innerText = m;
     seccondE1.innerText = s;
